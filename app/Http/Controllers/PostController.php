ed09e9b13f28e179da917610459135ec3a2affcd
EnriqueBobadilla <enriquebobadilla8@gmail.com>
2023-07-06 18:01:03 -0700

    migraciones y eloquent

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,20 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
     
     public function index()
     {
-        $posts = [
-            ['title' => 'Primer post'],
-            ['title' => 'Segundo post'],
-            ['title' => 'Tercer post'],
-            ['title' => 'Cuarto post']
-
-        ];
+        $posts =  Post::get();
 
         return view('blog', ['posts' => $posts]);
 
